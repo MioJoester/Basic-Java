28ccda056fd2d2a8a16d0d1d1629593a98b1e1cd
141Taskstalker <mananthaker2505@gmail.com>
2025-08-24 22:02:28 +0530

    Create condition.java

---
+++ condition.java
@@ -0,0 +1,14 @@
+public class SquareOrCube {
+    public static void main(String[] args) {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+
+        if (a > b) {
+            System.out.println(a * a);          // square of first
+        } else if (b > a) {
+            System.out.println(b * b * b);      // cube of second
+        } else {
+            System.out.println("Both are equal");
+        }
+    }
+}
