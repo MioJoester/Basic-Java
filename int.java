dc753b49189b1c8fc98e137663fabaf6f9815b09
141Taskstalker <mananthaker2505@gmail.com>
2025-08-24 20:42:56 +0530

    Create int.java

---
+++ int.java
@@ -0,0 +1,6 @@
+public class SimpleInterest {
+    public static void main(String[] args) {
+        double p = 1000, r = 5, t = 2;
+        System.out.println("Simple Interest: " + (p * r * t / 100));
+    }
+}
